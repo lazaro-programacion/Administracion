1ae9dbc96a247a751f80f8857115174baf2978b9
lazaro-programacion <you@example.com>
2020-02-12 09:49:46 +0100

    wwç

---
+++ Administracion-master/src/main/java/com/lazaro/Administracion/AdministracionApplication.java
@@ -0,0 +1,15 @@
+package com.lazaro.Administracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministracionApplication.class, args);
+
+	}
+
+	
+}
---
+++ Administracion-master/src/main/java/com/lazaro/Administracion/controller/MainController.java
@@ -0,0 +1,70 @@
+package com.lazaro.Administracion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * MainController
+ */
+@Controller
+public class MainController {
+
+    @GetMapping ("/")
+    @ResponseBody
+    public String holaMundo(@RequestParam("nombre") String name, @RequestParam("edad") Integer edad){
+        return " Hola: "+ name +" Tienes: "+ edad + " años";
+    }
+
+    @GetMapping ("/nuevo")
+    public ModelAndView helloHTML(){
+
+        ModelAndView respuesta = new ModelAndView("hello");
+            String msg = "Hola Mundo html";
+            respuesta.addObject("mensaje", msg);
+                return respuesta;
+    }
+    @GetMapping ("/calculadora")
+    @ResponseBody
+    public String calculadoraHTML(@RequestParam("operando1") Integer operando1, @RequestParam("operando2") Integer operando2, 
+    @RequestParam("operacion") String operacion){
+
+        
+        double resultado =0.0;
+        
+         
+
+        switch (operacion) {
+            case "suma":
+                resultado= operando1 + operando2;
+                break;
+            case "resta":
+                resultado=operando1 - operando2;
+                break;
+            case "multiplicacion":
+                resultado=operando1 * operando2;
+                break;
+            case "division":
+                resultado=operando1 / operando2;
+                break;
+            case "^":
+                resultado=(int)Math.pow(operando1, operando2);
+                break;
+            case "%":
+                resultado=operando1 % operando2;
+                break;
+            default:
+                return "No coinciden los operadores";
+            
+           
+           }
+
+           return operando1+" "+operacion+" "+operando2+" = "+resultado;
+	
+        
+        }
+    }
+      
+
